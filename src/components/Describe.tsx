b58b6c2808a0980292264bbb88000f407db6e09a
TranVanTienDat <ckim32993@gmail.com>
2023-10-05 22:11:58 +0700

    add

---
+++ src/components/Describe.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { describeProps } from "../interface";
+import { Button, Heading } from ".";
+import { Frame } from "./Frame";
+
+export const Describe = ({ image, title, text }: describeProps) => {
+  return (
+    <Frame>
+      <img src={image} alt="" className="h-[400px]" />
+      <div>
+        <Heading title={title} text={text} />
+        <div className="mt-8">
+          <Button title="Learn More" classes="bg-green text-colorWhite" />
+        </div>
+      </div>
+    </Frame>
+  );
+};
